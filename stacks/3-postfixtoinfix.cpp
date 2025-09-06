0ee83947bad045920c9df435a459532b77f4597e
dhiraj <dhirajwagh2408@gmail.com>
2025-09-07 00:23:16 +0530

    postfix to infix conversion

---
+++ stacks/3-postfixtoinfix.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+#include<stack>
+#include<string>
+using namespace std;
+
+string postfixToInfix(string s){
+    stack<string> st;
+    for(int i = 0; i < s.size(); i++){
+        if(isalnum(s[i])){
+            st.push(string(1, s[i]));
+        }
+        else{
+            string t1 = st.top();
+            st.pop();
+            string t2 = st.top();
+            st.pop();
+            string con = '(' + t2 + s[i] + t1 + ')';
+            st.push(con);
+        }
+    }
+    return st.top();
+}
+
+int main(){
+    string postfix = "AB-DE+F*/";
+    string result = postfixToInfix(postfix);
+    cout<<result;
+    return 0;
+}
